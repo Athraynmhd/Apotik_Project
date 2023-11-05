37d3a165e7f7ced5d5facfb0bc5ec602ec901835
Athar Rayyan Muhammad <132254139+Athraynmhd@users.noreply.github.com>
2023-11-05 15:14:03 +0700

    Update ObatSakitHati.java

--- ObatSakitHati.java
+++ ObatSakitHati.java
@@ -1,4 +1,7 @@
 class ObatSakitHati extends Obat {
      private double harga;
      
-    
+    ublic ObatSakitHati(int stadium,  double harga) {
+        super(namaObat, "Obat ini digunakan untuk meredakan sakit hati dan stress. Tidak berlaku kalo frendly", stadium);
+        this.harga = harga;
+    }
