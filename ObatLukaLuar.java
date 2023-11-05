361e8f46378e75e87a725a29cea12a927f2ba7e3
Athar Rayyan Muhammad <132254139+Athraynmhd@users.noreply.github.com>
2023-11-05 15:38:56 +0700

    Update ObatLukaLuar.java

--- ObatLukaLuar.java
+++ ObatLukaLuar.java
@@ -2,22 +2,10 @@ class ObatLukaLuar extends Obat {
     public ObatLukaLuar(int stadium, String namaObat) {
         super(namaObat, stadium);
     }
-/**
-  public double getHarga() {
-        return harga;
-    } */
 
-  @Override
+    @Override
     public String dosis() {
-        switch (stadium) {
-            case 1: 
-                return "Dosis ringan: Oleskan pada luka sekali sehari.";
-            case 2: 
-                return "Dosis sedang: Oleskan pada luka dua kali sehari.";
-            case 3: 
-                return "Dosis berat: Oleskan pada luka tiga kali sehari.";
-            default: 
-                return "Stadium tidak dikenal.";
-        }
+        // Implementasi khusus untuk dosis obat luka luar
+        return "Dosis obat luka luar: //dosis berdasarkan stadium , kalo ringan sesuaikan aja biar ga meninggoy";
     }
 }
