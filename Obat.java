b7b590e9404857349e2ecdcc4c223cc945cc9eab
Athar Rayyan Muhammad <132254139+Athraynmhd@users.noreply.github.com>
2023-11-05 13:47:17 +0700

    Update Obat.java

--- Obat.java
+++ Obat.java
@@ -3,3 +3,9 @@ public abstract class Obat {
     protected String deskripsi;
     protected int stadium;
     private double harga;
+
+    public Obat(String nama, String deskripsi, int stadium) {
+        this.nama = nama;
+        this.deskripsi = deskripsi;
+        this.stadium = stadium;
+    }    
