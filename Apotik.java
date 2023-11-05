381e902a6e327928da68903a8559a89da7a77315
Athar Rayyan Muhammad <132254139+Athraynmhd@users.noreply.github.com>
2023-11-05 13:40:29 +0700

    Update Apotik.java

--- Apotik.java
+++ Apotik.java
@@ -38,3 +38,10 @@ public class Apotik {
             if (stadium == 1) {
                 System.out.println("Untuk stadium 1, tidak ada obat yang diperlukan. Disarankan minum air hangat dan cukup istirahat.");
             } else {
+                String[] opsiObat = getOpsiObat(jenis, stadium);
+                System.out.println("Pilih obat:");
+                for (int i = 0; i < opsiObat.length; i++) {
+                    System.out.println((i + 1) + ". " + opsiObat[i]);
+                }
+                int pilihanObat = Integer.parseInt(scanner.nextLine());
+                String obatTerpilih = opsiObat[pilihanObat - 1]; }
