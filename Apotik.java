fabc501e7fb893168ed1a5c24ad148452a3ff316
Athar Rayyan Muhammad <132254139+Athraynmhd@users.noreply.github.com>
2023-11-05 13:41:33 +0700

    Update Apotik.java

--- Apotik.java
+++ Apotik.java
@@ -42,4 +42,7 @@ public class Apotik {
                 System.out.println("Pilih obat:");
                 for (int i = 0; i < opsiObat.length; i++) {
                     System.out.println((i + 1) + ". " + opsiObat[i]);
+                }
+                int pilihanObat = Integer.parseInt(scanner.nextLine());
+                String obatTerpilih = opsiObat[pilihanObat - 1];
                 
