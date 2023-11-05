2140af15bc1be77a850e0a42aea4c2b2d9e354b3
Athar Rayyan Muhammad <132254139+Athraynmhd@users.noreply.github.com>
2023-11-05 15:28:44 +0700

    Update ObatSakitHati.java

--- ObatSakitHati.java
+++ ObatSakitHati.java
@@ -1,7 +1,4 @@
 class ObatSakitHati extends Obat {
-     private double harga;
-     
-    ublic ObatSakitHati(int stadium,  double harga) {
-        super(namaObat, "Obat ini digunakan untuk meredakan sakit hati dan stress. Tidak berlaku kalo frendly", stadium);
-        this.harga = harga;
+    public ObatSakitHati(int stadium, String namaObat) {
+        super(namaObat, "Obat ini digunakan untuk meredakan sakit hati dan stres.", stadium);
     }
