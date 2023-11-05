d71fdf9e4f151f61d80f2ca3dfba63967683f751
Athar Rayyan Muhammad <132254139+Athraynmhd@users.noreply.github.com>
2023-11-05 13:27:57 +0700

    Add files via upload

---
+++ Apotik.java
@@ -0,0 +1,11 @@
+import java.text.DecimalFormat; // digunakan untuk mengubah format angka menjadi tampilan yang lebih terstruktur dan mudah dibaca
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Apotik {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Kasir kasir = new Kasir();
+        String namaPelanggan;
+        boolean adaKeluhanLain;
