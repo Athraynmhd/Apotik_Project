4b95d49edb6dc81e62dea0b77644a31d2d6e66c5
Athar Rayyan Muhammad <132254139+Athraynmhd@users.noreply.github.com>
2023-11-05 13:30:40 +0700

    Update Apotik.java

--- Apotik.java
+++ Apotik.java
@@ -15,7 +15,10 @@ public class Apotik {
         namaPelanggan = scanner.nextLine();
 
         do {
+            System.out.println("Pilih jenis sakit:");
             System.out.println("1. Sakit Kepala");
             System.out.println("2. Demam");
             System.out.println("3. Luka Luar");
             System.out.println("4. Luka Dalam");
+            System.out.println("5. Sakit Hati");
+            System.out.println("0. Selesai dan cetak struk");
