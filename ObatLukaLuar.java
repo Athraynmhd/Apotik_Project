d6d51f1d127cc07ca32b47d7712f44698b8a781a
Athar Rayyan Muhammad <132254139+Athraynmhd@users.noreply.github.com>
2023-11-05 15:15:07 +0700

    Update ObatLukaLuar.java

--- ObatLukaLuar.java
+++ ObatLukaLuar.java
@@ -1,7 +1,7 @@
 class ObatLukaLuar extends Obat {
      private double harga;
      
-  ObatLukaLuar(int stadium, String namaObat, double harga) {
+  public ObatLukaLuar(int stadium, String namaObat, double harga) {
         super(namaObat, "Obat ini digunakan untuk merawat luka luar.", stadium);
         this.harga = harga;
     }
