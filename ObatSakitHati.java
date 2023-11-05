f991a9f9c234721ed1b0ca59a46bc78d70858638
Athar Rayyan Muhammad <132254139+Athraynmhd@users.noreply.github.com>
2023-11-05 15:29:33 +0700

    Update ObatSakitHati.java

--- ObatSakitHati.java
+++ ObatSakitHati.java
@@ -2,3 +2,8 @@ class ObatSakitHati extends Obat {
     public ObatSakitHati(int stadium, String namaObat) {
         super(namaObat, "Obat ini digunakan untuk meredakan sakit hati dan stres.", stadium);
     }
+     @Override
+    public String dosis() {
+        return "Dosis obat sakit hati: [Detail dosis berdasarkan stadium]"; // tapi tergantung pemabagian di apotik
+    }
+}
