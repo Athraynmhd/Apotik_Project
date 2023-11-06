ef403bbe44b31a7c1c7a8e8a9ca7658fc568ef34
Athar Rayyan Muhammad <132254139+Athraynmhd@users.noreply.github.com>
2023-11-06 10:23:39 +0700

    Update ObatDemam.java

--- ObatDemam.java
+++ ObatDemam.java
@@ -2,7 +2,7 @@ class ObatDemam extends Obat {
     private double harga;
 
     public ObatDemam(int stadium, String namaObat, double harga) {
-        super(namaObat, "Obat ini digunakan untuk menurunkan demam.", stadium);
+        super(namaObat, "Obat ini digunakan untuk menurunkan demam.", stadium); 
         this.harga = harga;
     }
 
