07528f70baf0bd2f9ad870b3795959b98edfe60d
Athar Rayyan Muhammad <132254139+Athraynmhd@users.noreply.github.com>
2023-11-06 10:25:50 +0700

    Update ObatLukaDalam.java

--- ObatLukaDalam.java
+++ ObatLukaDalam.java
@@ -14,7 +14,7 @@ class ObatLukaDalam extends Obat {
     public String dosis() {
         switch (stadium) {
             case 1: 
-                return "Dosis ringan: 1 tablet setiap 12 jam.";
+                return "Dosis ringan: 1 tablet setiap 12 jam."; // Luka dalam kalo ga make sense dalam bentuk tablet, bisa ganti dengan obat kumur, let's say 1 kali kumur
             case 2: 
                 return "Dosis sedang: 1 tablet setiap 8 jam.";
             case 3: 
