7b7ffa7a0befc71d14d08694f812b34a381a3a27
Alfi Zamriza <130981845+alfizamriza@users.noreply.github.com>
2023-11-09 12:51:57 +0700

    Update DataKasir.java

--- DataKasir.java
+++ DataKasir.java
@@ -5,6 +5,9 @@ public class DataKasir extends Kasir
         if ("K001".equals(IdKasir)) {
             this.NamaKasir = "Aria Grande";
             this.IdKasir = "K001";
+        } else if ("K002".equals(IdKasir)) {
+            this.NamaKasir = "Nagaragana";
+            this.IdKasir = "K002";
         }
      }
    
